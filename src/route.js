a428263b85d814c6d64497b245a9762be9e29727
心叶 <yelloxing@gmail.com>
2020-05-27 11:17:05 +0800

    更新有危险的node包

--- src/route.js
+++ src/route.js
@@ -3,19 +3,19 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-// 引入首页
-import Home from './pages/Home.vue';
+// 引入菜单
+import Menu from './pages/menu.vue';
 
 // 配置路由
 const router = new VueRouter({
   routes: [
     {
-      path: '/home',
-      component: Home
+      path: '/menu',
+      component: Menu
     },
     {
       path: '/*',
-      redirect: 'home'
+      redirect: 'menu'
     }]
 });
 
